2bc3dd04202e4d8c024d51aaf515909c71429e1b
truth <truth@mba.local>
2021-05-20 20:53:22 +0900

    Platformモジュールの使用

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, ViewStyle } from 'react-native';
+import { View, StyleSheet, Text, Platform } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,31 +7,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  myComponent: {
-    width: 256,
-    height: 32,
-  }
 });
 
-interface Props {
-  style?: ViewStyle;
-}
-function MyCompnent(props: Props) {
-  const style = React.useMemo(
-    () => StyleSheet.compose(styles.myComponent, props.style),
-    [props.style],
-  );
-  return (
-    <View style={style}>
-      <Text>スタイル指定可能なコンポーネント</Text>
-    </View>
-  );
-}
+const label = Platform.select({
+  ios: 'iOS',
+  android: 'Android',
+});
+
+const onPress = Platform.select({
+  ios: () => {
+    console.warn(Platform.OS, Platform.Version, Platform.isTV)
+  },
+  android: () => {
+    console.warn(Platform.OS, Platform.Version, Platform.isTV)
+  }
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MyCompnent style={{backgroundColor: 'red'}} />
+      <Text onPress={onPress}>{label}</Text>
     </View>
   );
 }
